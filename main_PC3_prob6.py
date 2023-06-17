fe4a6fbf4585ec5cc5765d9c5d44a13d7d2f0697
AdrianAramburu <126032767+AdrianAramburu@users.noreply.github.com>
2023-06-16 22:34:46 -0500

    main_subida6

---
+++ main_PC3_prob6.py
@@ -0,0 +1,9 @@
+import PC3_prob6
+
+a = 10
+b = 2
+
+print("Suma:", PC3_prob6.suma(a, b))
+print("Resta:", PC3_prob6.resta(a, b))
+print("Producto:", PC3_prob6.producto(a, b))
+print("División:", PC3_prob6.division(a, b))
